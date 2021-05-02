0e073a1c53378078d58f69b6463100a2be39a0e9
Vidit Vora <viditvora116@gmail.com>
2021-05-02 15:16:06 +0530

    send mail only when current email is different than previous mail

--- handler.js
+++ handler.js
@@ -1,34 +1,77 @@
 const fetch = require('node-fetch');
 const nodemailer = require('nodemailer');
 const moment = require('moment');
+const crypto = require('crypto');
+const { SSMClient, GetParameterCommand, PutParameterCommand } = require("@aws-sdk/client-ssm");
+
+const ssmClient = new SSMClient({ region: 'ap-south-1' });
+const ssmKey = process.env.PREVIOUS_EMAIL_TEXT;
 
 const mailTransporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'cowinmailer@gmail.com',
+    user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
   }
 });
 
 const mailDetails = {
-  from: 'cowinmailer@gmail.com',
+  from: process.env.GMAIL_USER,
   to: 'viditvora116@gmail.com',
   subject: 'Vaccine Availability'
 };
 
+const getHash = (message) => crypto.createHash('md5').update(message).digest('hex');
+
+const getPreviousEmailText = async () => {
+  const params = {
+    Name: ssmKey
+  };
+  const getParameter = new GetParameterCommand(params);
+  try {
+    const data = await ssmClient.send(getParameter);
+    if (data.Parameter?.Value) {
+      return data.Parameter.Value;
+    }
+    return null;
+  } catch (error) {
+    if (error.name === 'ParameterNotFound') {
+      return null;
+    }
+    throw error;
+  }
+}
+
+const saveCurrentEmailText = async (hashedEmailText) => {
+  const params = {
+    Value: hashedEmailText,
+    DataType: 'text',
+    Name: ssmKey,
+    Type: 'String',
+    Overwrite: true
+  };
+  const putParameter = new PutParameterCommand(params);
+  try {
+    await ssmClient.send(putParameter);
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
 const constructMailText = (data) => {
   let text = '';
   data.forEach(center => {
-    text = text + `Center Name: ${center.name}  Pincode: ${center.pincode}  Free: ${center.fee_type === 'Paid' ? 'No' : 'Yes'} \n`;
+    text += `Center Name: ${center.name}  Pincode: ${center.pincode}  Free: ${center.fee_type === 'Paid' ? 'No' : 'Yes'} \n`;
     center.sessions.forEach((session) => {
-      text = text + `Date: ${session.date}\n`;
-      text = text + `Available Capacity: ${session.available_capacity}  Age Limit: ${session.min_age_limit === 18 ? '18-44' : '44+'}  `;
+      text += `Date: ${session.date}\n`;
+      text += `Available Capacity: ${session.available_capacity}  Age Limit: ${session.min_age_limit === 18 ? '18-44' : '44+'}  `;
       if (session.vaccine) {
-        text = text + `Vaccine: ${session.vaccine}`;
+        text += `Vaccine: ${session.vaccine}`;
       }
-      text = text + '\n';
+      text += '\n';
     });
-    text = text + '\n\n';
+    text += '\n\n';
   });
   return text;
 };
@@ -60,11 +103,20 @@ module.exports.mailer = async (event, context) => {
     result = result.filter(r => !!r);
 
     if (result.length) {
-      const info = await mailTransporter.sendMail({
-        ...mailDetails,
-        text: constructMailText(result)
-      });
-      console.log('Message sent: %s', info.messageId);
+      const currentEmailText = constructMailText(result);
+      const previousEmailTextHash = await getPreviousEmailText();
+      const currentEmailTextHash = getHash(currentEmailText);
+
+      if (currentEmailTextHash !== previousEmailTextHash) {
+        await saveCurrentEmailText(currentEmailTextHash);
+        const info = await mailTransporter.sendMail({
+          ...mailDetails,
+          text: currentEmailText
+        });
+        console.log('Message sent: %s', info.messageId);
+      } else {
+        console.log('No new availability added since last sent email');
+      }
     } else {
       console.log('Everything Booked!');
     }
