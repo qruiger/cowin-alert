32b7de0ffe6837b047e057e3787bfb19148c2ea4
qruiger <viditvora116@gmail.com>
2021-05-03 18:13:11 +0530

    Update subscriber.js

--- src/subscriber.js
+++ src/subscriber.js
@@ -1,15 +1,11 @@
 module.exports = {
-  email: 'viditvora116@gmail.com',
+  email: 'abc@gmail.com',
   filters: {
     districtId: 395, // Mumbai
     preferredPincodes: [
-      400053,
-      400058,
-      400059,
-      400064,
-      400093,
-      400102,
-      400104
+      400001,
+      400002,
+      400003
     ]
   }
 };
