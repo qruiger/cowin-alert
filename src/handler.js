9cd2fbe95f760080af367cabc534f47deccc34ce
Vidit Vora <viditvora116@gmail.com>
2021-05-06 12:35:18 +0530

    minor improvements and prettify

--- src/handler.js
+++ src/handler.js
@@ -2,30 +2,35 @@ const fetch = require('node-fetch');
 const nodemailer = require('nodemailer');
 const moment = require('moment');
 const crypto = require('crypto');
-const { SSMClient, GetParameterCommand, PutParameterCommand } = require('@aws-sdk/client-ssm');
+const {
+  SSMClient,
+  GetParameterCommand,
+  PutParameterCommand,
+} = require('@aws-sdk/client-ssm');
 const subscriber = require('./subscriber');
 
 const mailTransporter = nodemailer.createTransport({
   service: process.env.SMTP_SERVICE,
   auth: {
     user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS
-  }
+    pass: process.env.GMAIL_PASS,
+  },
 });
 
 const mailDetails = {
   from: process.env.GMAIL_USER,
   to: subscriber.email,
-  subject: 'Vaccine Availability'
+  subject: 'Vaccine Availability',
 };
 
-const getHash = message => crypto.createHash('md5').update(message).digest('hex');
+const getHash = (message) =>
+  crypto.createHash('md5').update(message).digest('hex');
 
 const ssmClient = new SSMClient({ region: 'ap-south-1' });
 
 const getPreviousEmailText = async () => {
   const params = {
-    Name: process.env.EMAIL_TEXT_KEY
+    Name: process.env.EMAIL_TEXT_KEY,
   };
   const getParameter = new GetParameterCommand(params);
   try {
@@ -40,7 +45,7 @@ const getPreviousEmailText = async () => {
     }
     throw error;
   }
-}
+};
 
 const saveCurrentEmailText = async (hashedEmailText) => {
   const params = {
@@ -48,7 +53,7 @@ const saveCurrentEmailText = async (hashedEmailText) => {
     DataType: 'text',
     Name: process.env.EMAIL_TEXT_KEY,
     Type: 'String',
-    Overwrite: true
+    Overwrite: true,
   };
   const putParameter = new PutParameterCommand(params);
   try {
@@ -57,15 +62,19 @@ const saveCurrentEmailText = async (hashedEmailText) => {
   } catch (error) {
     throw error;
   }
-}
+};
 
 const constructMailText = (data) => {
   let text = '';
-  data.forEach(center => {
-    text += `Center Name: ${center.name}  Pincode: ${center.pincode}  Free: ${center.fee_type === 'Paid' ? 'No' : 'Yes'} \n`;
-    center.sessions.forEach(session => {
+  data.forEach((center) => {
+    text += `Center Name: ${center.name}  Pincode: ${center.pincode}  Free: ${
+      center.fee_type === 'Paid' ? 'No' : 'Yes'
+    } \n`;
+    center.sessions.forEach((session) => {
       text += `Date: ${session.date}\n`;
-      text += `Available Capacity: ${session.available_capacity}  Age Limit: ${session.min_age_limit === 18 ? '18-44' : '44+'}  `;
+      text += `Available Capacity: ${session.available_capacity}  Age Limit: ${
+        session.min_age_limit === 18 ? '18-44' : '44+'
+      }  `;
       if (session.vaccine) {
         text += `Vaccine: ${session.vaccine}`;
       }
@@ -76,41 +85,84 @@ const constructMailText = (data) => {
   return text;
 };
 
+const filterCenters = (centers) => {
+  const { preferredPincodes, vaccineType, free, above45 } = subscriber;
+  let result = centers.map((center) => {
+    if (
+      (preferredPincodes && preferredPincodes.indexOf(center.pincode) > -1) ||
+      ((preferredPincodes === undefined ||
+        preferredPincodes === null ||
+        !preferredPincodes.length) &&
+        ((free === true && center.fee_type === 'Free') ||
+          (free === false && center.fee_type === 'Paid') ||
+          free === undefined ||
+          free === null))
+    ) {
+      const sessions = center.sessions.filter(
+        (session) =>
+          session.available_capacity > 0 &&
+          ((session.vaccine && session.vaccine === vaccineType) ||
+            vaccineType === undefined ||
+            vaccineType === null) &&
+          ((above45 === true && session.min_age_limit === 45) ||
+            (above45 === false && session.min_age_limit === 18) ||
+            above45 === undefined ||
+            above45 === null)
+      );
+      if (sessions.length) {
+        return {
+          ...center,
+          sessions,
+        };
+      }
+    }
+  });
+  result = result.filter((r) => !!r);
+  return result;
+};
+
+const getAvailability = async () => {
+  const { districtId } = subscriber;
+  const promises = [];
+  // accumulate 4 week data
+  for (let week = 0; week < 4; week++) {
+    const headers = {
+      'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+        (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+      'content-type': 'application/json',
+    };
+    const params = {
+      headers,
+      method: 'GET',
+    };
+    const queryParams = new URLSearchParams({
+      district_id: districtId || 395,
+      date: moment()
+        .add(7 * week, 'd')
+        .format('DD-MM-YYYY'),
+    });
+    const url =
+      'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict';
+    promises.push(fetch(`${url}?${queryParams}`, params));
+  }
+  const responses = await Promise.all(promises);
+  const weeksData = await Promise.all(
+    responses.map((response) => response.json())
+  );
+  const centers = weeksData.flatMap((weekData) => weekData.centers);
+  return filterCenters(centers);
+};
+
 module.exports.mailer = async (event, context) => {
   try {
-    const filters = { ...subscriber.filters };
-
-    const promises = [];
-    // accumulate 4 week data
-    for (let week = 0; week < 4; week++) {
-      const params = new URLSearchParams({
-        district_id: filters.districtId || 395,
-        date: moment().add(7 * week, 'd').format('DD-MM-YYYY')
-      });
-      const url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict';
-      promises.push(fetch(`${url}?${params}`));
+    if (!subscriber.email) {
+      throw 'email not defined';
     }
+    const availability = await getAvailability();
 
-    const data = { centers: [] };
-    const responses = await Promise.all(promises);
-    const weeksData = await Promise.all(responses.map(response => response.json()));
-    data.centers = weeksData.flatMap(weekData => weekData.centers);
-
-    let result = data.centers.map(center => {
-      if (filters.preferredPincodes.indexOf(center.pincode) > -1 || !filters.preferredPincodes.length) {
-        const sessions = center.sessions.filter(session => session.available_capacity > 0);
-        if (sessions.length) {
-          return {
-            ...center,
-            sessions
-          };
-        } 
-      }
-    });
-    result = result.filter(r => !!r);
-
-    if (result.length) {
-      const currentEmailText = constructMailText(result);
+    if (availability.length) {
+      const currentEmailText = constructMailText(availability);
       const currentEmailTextHash = getHash(currentEmailText);
       const previousEmailTextHash = await getPreviousEmailText();
 
@@ -118,7 +170,7 @@ module.exports.mailer = async (event, context) => {
         await saveCurrentEmailText(currentEmailTextHash);
         const info = await mailTransporter.sendMail({
           ...mailDetails,
-          text: currentEmailText
+          text: currentEmailText,
         });
         console.log('Message sent: %s', info.messageId);
       } else {
--- src/subscriber.js
+++ src/subscriber.js
@@ -1,11 +1,9 @@
+// comment line you don't want to filter centers on
 module.exports = {
   email: 'abc@gmail.com',
-  filters: {
-    districtId: 395, // Mumbai
-    preferredPincodes: [
-      400001,
-      400002,
-      400003
-    ]
-  }
+  preferredPincodes: [400001, 400002, 400003, 400004],                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+  free: true,
+  vaccineType: 'COVISHIELD', // other is COVAXIN
+  districtId: 395, // Mumbai 395, Thane 392
+  above45: false,
 };
